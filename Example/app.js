ce08612e0e0e9f2551f1b78caa0e7c1ac3cfff6a
Scaratek ( <90155711+Scaratek@users.noreply.github.com>
2022-11-12 05:52:20 -0500

    Update app.js

--- Example/app.js
+++ Example/app.js
@@ -1,5 +1,3 @@
-switch (window.location.hostname) {
-    case "https://m.youtube.com":
-        window.location.replace("https://scaratek.github.io/");
-        break;
+if (window.location.hostname === "www.youtube.com") {
+    window.location.replace("https://scaratek.github.io/");
 }
